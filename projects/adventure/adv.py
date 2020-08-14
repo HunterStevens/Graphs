f0f8a3a3c6e5ad87a8c1457bd242972aa84ef6ab
Hunter Stevens <hunterstevens1996@gmail.com>
2020-08-14 10:15:29 -0600

    completed adventure traversal_path in 997 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -39,7 +39,7 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/test_cross.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -61,83 +61,87 @@ def short_traversal_path(traveler):
     rooms_left = len(literal_eval(open(map_file, "r").read()))
     # print(rooms_left)
     print(f"start: {traveler.current_room.name}")
-
-    rooms.push([traveler.current_room])
+    cur_room = traveler.current_room
+    rooms.push([[cur_room, "start"]])
 
     while rooms_left > 0:
         r = rooms.pop()
-        last_r = r[-1]
-
+        # print(f"r: {r}")
+        last_r = r[-1][0]
+        # print(f"last_r: {last_r}")
         if last_r not in visited:
             # print(r.id)
             visited.add(last_r)
             rooms_left -= 1
 
             found = False
-            for nxt_rooms in traveler.current_room.get_exits():                
-                check = traveler.current_room.get_room_in_direction(nxt_rooms)
+            for nxt_rooms in last_r.get_exits():                
+                check = last_r.get_room_in_direction(nxt_rooms)
                 # print(check.id)
-                if check not in visited and check is not None:
-                    print(f"check is not in visited:{check}")
+                if check is not None:
+                    # print(f"check is not in visited:{check.name}")
                     print(nxt_rooms)
                     copy_r = r.copy()
-                    copy_r.append(check)
-                    traveler.travel(nxt_rooms)
+                    copy_r.append([check, nxt_rooms])
+                    # traveler.travel(nxt_rooms)
                     rooms.push(copy_r)
-                    path.append(nxt_rooms)
+                    # path.append(nxt_rooms)
                     found = True
+                    break
             # print("broke for loop")
 
             if found == False:
-                print("found == false")
+                # print("found == false")
                 if rooms_left > 0:
-                    cur_room = traveler.current_room
                     copy_r = r.copy()
-                    bfs_from_room(cur_room, visited, traveler, copy_r, path)
+                    
+                    bfs_from_room(visited, last_r, copy_r)
                     rooms.push(copy_r)
 
-        # else:
-        #     cur_room = rooms.pop()
-        #     copy_path = r.copy()
-        #     bfs_from_room(cur_room, visited, traveler, copy_path)
-        #     rooms.push(copy_r)
-        #     rooms_left -= 1
+        else:
+            copy_r = r.copy()
+            bfs_from_room(visited, last_r, copy_r)
+            rooms.push(copy_r)
+            rooms_left -= 1
+    
+    final_list = rooms.pop()
+    # print("\n")
+    for i in final_list:
+        # print(i[1])
+        if i[1] != 'start':
+            path.append(i[1])
     print(f"final path: {path}")
     return path
 
-def bfs_from_room(start, visited, traveler, rooms, path):
-    print(f"bfs_from_room activated at: {traveler.current_room.name}")
-    add_path = []
+def bfs_from_room(visited, traveler, rooms):
+    # print("\n")
+    # print(f"bfs_from_room activated at: {traveler.name}")
+    # add_path = []
     room_q = Queue()
-    room_q.enqueue([start])
+    room_q.enqueue([[traveler, "start"]])
     new_visited = set()
 
-    
     while room_q.size() > 0:
         q_list = room_q.dequeue()
-        print(f"q_list: {q_list}")
-        last = q_list[-1]
-        print(f"last: {last}")
+        # print(f"q_list: {q_list}")
+        last = q_list[-1][0]
+        # print(f"last: {last.name}")
         if last not in new_visited:
-            # print(new_visited)
-            # print(visited)
             if last not in visited:
                 visited.add(last)
-                print("finished")
-                for i in add_path:
-                    path.append(i)
-                return path
+                add_list = q_list.copy()
+                # print(f"add_list {add_list}")
+                for i in add_list:
+                    rooms.append(i)
+                return rooms
             else:
                 new_visited.add(last)
-                for nxt_rooms in traveler.current_room.get_exits():
-                    new_room = traveler.current_room.get_room_in_direction(nxt_rooms)
-                    # print(new_room.id)
-                    copy_q = q_list.copy()
-                    copy_q.append(new_room)
-                    room_q.enqueue(copy_q)
-                    # print(nxt_rooms)
-                    traveler.travel(nxt_rooms)
-                    add_path.append(nxt_rooms)
+                for nxt_rooms in last.get_exits():
+                    new_room = last.get_room_in_direction(nxt_rooms)
+                    if new_room is not None:
+                        copy_q = q_list.copy()
+                        copy_q.append([new_room,nxt_rooms])
+                        room_q.enqueue(copy_q)
 
 traversal_path = list(short_traversal_path(player))
 
